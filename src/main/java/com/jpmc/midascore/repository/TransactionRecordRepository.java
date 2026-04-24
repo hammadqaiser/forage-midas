d4863b0355d68ab59aa8a3ee0edcba8fe85e2223
hammadqaiser <hammadqais2@gmail.com>
2026-04-24 16:05:15 +0500

    Add interface logic for TransactionRecord class
    Signed-off-by: hammadqaiser <hammadqais2@gmail.com>

--- src/main/java/com/jpmc/midascore/repository/TransactionRecordRepository.java
+++ src/main/java/com/jpmc/midascore/repository/TransactionRecordRepository.java
@@ -1,4 +1,8 @@
 package com.jpmc.midascore.repository;
 
-public interface TransactionRecordRepository {
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TransactionRecordRepository extends CrudRepository<TransactionRecord, Long> {
 }
